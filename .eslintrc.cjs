70a813fe642f4045c60a1bd17a8bc33916a07746
Kirk Hodgman <kjhodgman@gmail.com>
2024-07-21 09:57:18 -0700

    update eslint and add prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,36 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json", "./tsconfig.app.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react-refresh"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+      },
+      {
+        usePrettierrc: false,
+      },
+    ],
   },
-}
+};
